a965dcb8db0c5d0a205a6f0936fdfba5b0b051db
farzanehbrz <f.barzegaridahaj>
2018-03-12 11:23:59 +0330

    log4g add dependency resources dir add HomeController add log4g define

--- src/main/java/com/nicico/controllers/HomeController.java
+++ src/main/java/com/nicico/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.nicico.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,9 +13,13 @@ Year: 2018
 */
 @Controller
 public class HomeController {
+    final static Logger logger = Logger.getLogger(HomeController.class);
+
     @ResponseBody
     @RequestMapping("/home")
     public String dummyMethod(){
+        logger.info("this is test for log");
+
         return "Hello Spring mvc!!!";
     }
 }
